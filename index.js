22a958419e77c66a7638023c8bc0111a7af3ce78
ANANTTIWARI01 <bhaukalbaba45@gmail.com>
2025-03-01 23:59:43 +0530

    server added

--- index.js
+++ index.js
@@ -54,5 +54,5 @@ app.delete("/delete/:id", async (req,res)=>{
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port is ${port}`)
 })
